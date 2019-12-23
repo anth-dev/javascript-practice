fbb071cd6bbde16ec0c7d09e806f18622376f07a
Anthony Giles <omgliekpie@gmail.com>
2019-12-23 13:57:05 -0700

    put exercise info into correct file

--- javascript-practice.js
+++ javascript-practice.js
@@ -3,10 +3,17 @@ const body = document.querySelector('body');
 body.setAttribute('style', 'background-color: black; color: white;');
 // Don't remove anything above this comment
 
-const container = document.querySelector('#container');
-const content = document.createElement('div');
+// Exercise
 
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+// Add the following elements to the container using ONLY javascript - do not add any
+// HTML or CSS code.
 
-container.appendChild(content);
+// 1. a <p> with red text that says “Hey I’m red!”
+// 2. an <h3> with blue text that says “I’m a blue h3!”
+// 3. a <div> with a black border and pink background color with the following 
+// elements inside of it:
+//      * another <h1> that says “I’m in a div”
+//      * a <p> that says “ME TOO!”
+//      * Hint for this one: after creating the div with createElement, append the 
+//          <h1> and <p> to it before adding it to the container.
+
