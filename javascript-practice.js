c8d404809ff91911bf2455d54d924f0c4f68dca2
Anthony Giles <omgliekpie@gmail.com>
2019-12-23 14:27:40 -0700

    add another div and style it

--- javascript-practice.js
+++ javascript-practice.js
@@ -10,7 +10,7 @@ body.setAttribute('style', 'background-color: black; color: white;');
 
 // 1. a <p> with red text that says “Hey I’m red!”
 // 2. an <h3> with blue text that says “I’m a blue h3!”
-// 3. a <div> with a black border and pink background color with the following 
+// 3. a <div> with a cornflowerblue border and pink background color with the following 
 // elements inside of it:
 //      * another <h1> that says “I’m in a div”
 //      * a <p> that says “ME TOO!”
@@ -20,10 +20,16 @@ body.setAttribute('style', 'background-color: black; color: white;');
 const container = document.querySelector('#container');
 const p = document.createElement('p');
 const h3 = document.createElement('h3');
+const div = document.createElement('div');
 
 p.style.cssText = 'color: red';
 p.textContent = 'Hey I\'m red!';
 container.appendChild(p);
+
 h3.style.cssText = 'color: blue';
 h3.textContent = 'I\'m a blue h3!';
 container.appendChild(h3);
+
+
+div.style.cssText = 'border-style: solid; border-color: cornflowerblue; background-color: pink'
+container.appendChild(div);
