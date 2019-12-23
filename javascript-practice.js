0ed5861ed4f00a17369325dcc1144277b1c0cdcb
Anthony Giles <omgliekpie@gmail.com>
2019-12-23 13:53:14 -0700

    add information about current exercise

--- javascript-practice.js
+++ javascript-practice.js
@@ -2,3 +2,11 @@
 const body = document.querySelector('body');
 body.setAttribute('style', 'background-color: black; color: white;');
 // Don't remove anything above this comment
+
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
