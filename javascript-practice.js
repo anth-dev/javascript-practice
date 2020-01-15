e56de0d92e7c234c0b557444cc83e34cb46190c6
Anthony Giles <omgliekpie@gmail.com>
2020-01-14 19:03:25 -0700

    make the headings

--- javascript-practice.js
+++ javascript-practice.js
@@ -4,3 +4,32 @@ body.style.cssText = 'background-color: black; color: white;';
 // Don't remove anything above this comment
 
 // Exercise
+
+const MOUNTAINS = [
+    {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+    {name: "Everest", height: 8848, place: "Nepal"},
+    {name: "Mount Fuji", height: 3776, place: "Japan"},
+    {name: "Vaalserberg", height: 323, place: "Netherlands"},
+    {name: "Denali", height: 6168, place: "United States"},
+    {name: "Popocatepetl", height: 5465, place: "Mexico"},
+    {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+  ];
+
+  // Your code here
+
+ function generateTableHead(table, data) {
+   let thead = table.createTHead();
+   let row = thead.insertRow();
+   for (let key of data) {
+       let th = document.createElement("th");
+       let text = document.createTextNode(key);
+       th.appendChild(text);
+       row.appendChild(th);
+   }
+ }
+ 
+ let table = document.querySelector("table");
+ let data = Object.keys(MOUNTAINS[0]);
+ generateTableHead(table, data);
+
+ 
